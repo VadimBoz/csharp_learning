74687d8551dd3595336c947b6704ab0a414ed57b
vadim <vadim3@mail.ru>
2022-08-06 14:18:24 +0300

    ex2

---
+++ exemple1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите имя ");
+string name=Console.ReadLine();
+
+if (name.ToLower()=="masha")
+   {
+    Console.WriteLine("привет masha");
+    }
+        else
+        {
+            Console.WriteLine("привет");
+        }
+
+
